3204d6ffe407dd1dce383adbaa2c1b6568fc8ad6
HugoC2510 <71280924+HugoC2510@users.noreply.github.com>
2020-11-02 15:39:37 +0100

    Update WorkGroup.cs

--- WorkGroup.cs
+++ WorkGroup.cs
@@ -10,10 +10,18 @@ namespace OOP_Project_Team13
     {
         public List<Student> members;
         public string name;
+        public Teacher professor;
         public WorkGroup(List<Student> _members, string _name)
         {
             this.members = _members;
             this.name = _name;
+        }
+        
+        public WorkGroup(List<Student> _members, string _name, Teacher _professor)
+        {
+            this.members = _members;
+            this.name = _name;
+            this.professor = _professor;
         }
     }
 }
